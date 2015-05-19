276345cb8168ae6b1fc2a0ca1e4d4a22b551d20a
Zhmayev Yaroslav <zhmayev.yaroslav@renderimage.it>
2015-05-19 10:05:34 +0200

    bumped assembly version and added NeutralResourcesLanguageAttribute attribute

--- Enterra.V8x1C/Properties/AssemblyInfo.cs
+++ Enterra.V8x1C/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 using System.Security;
 
@@ -37,5 +37,5 @@ using System.Security;
 
 [assembly: AllowPartiallyTrustedCallers]
 
-[assembly: AssemblyVersion("1.7.1")]
-[assembly: AssemblyFileVersion("1.7.1")]
+[assembly: AssemblyVersion("1.8.0.*")]
+[assembly: NeutralResourcesLanguageAttribute("ru")]
